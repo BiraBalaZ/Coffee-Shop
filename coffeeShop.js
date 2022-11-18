b2e4c3940b79781af982db310f62a87a257abd91
BiraBalaZ <85596186+BiraBalaZ@users.noreply.github.com>
2022-11-18 16:18:22 -0300

    Versão 1.0.3

--- coffeeShop.js
+++ coffeeShop.js
@@ -19,21 +19,21 @@ var menu = [
     item7  = {nome:'Latte               ', valor:'08.00', qnty: 0},
     item8  = {nome:'Duplo               ', valor:'05.00', qnty: 0},
     item9  = {nome:'Frappuccino         ', valor:'17.50', qnty: 0},
-    item10 = {nome:'Café com leite      ', valor:'04.00', qnty: 0},
-    item11 = {nome:'Leite               ', valor:'03.50', qnty: 0},
-    item12 = {nome:'X-Burger            ', valor:'25.00', qnty: 0},
-    item13 = {nome:'Donuts              ', valor:'06.00', qnty: 0},
-    item14 = {nome:'Misto quente        ', valor:'10.00', qnty: 0},
-    item15 = {nome:'Brigadeirão         ', valor:'05.00', qnty: 0},
-    item16 = {nome:'Torta de pêssego    ', valor:'07.50', qnty: 0},
-    item17 = {nome:'Torta de maçã       ', valor:'07.50', qnty: 0},
-    item18 = {nome:'Torta de morango    ', valor:'07.50', qnty: 0},
-    item19 = {nome:'Bolo de brigadeiro  ', valor:'04.50', qnty: 0},
-    item20 = {nome:'Bolo de chocolate   ', valor:'04.50', qnty: 0},
-    item21 = {nome:'Bolo de fubá        ', valor:'04.50', qnty: 0},
-    item22 = {nome:'Bolo de morango     ', valor:'04.50', qnty: 0},
-    item23 = {nome:'Pão de queijo (100g)', valor:'02.50', qnty: 0},
-    item23 = {nome:'Brownie             ', valor:'07.00', qnty: 0}
+    item10 = {nome:'Café com leite     ', valor:'04.00', qnty: 0},
+    item11 = {nome:'Leite              ', valor:'03.50', qnty: 0},
+    item12 = {nome:'X-Burger           ', valor:'25.00', qnty: 0},
+    item13 = {nome:'Donuts             ', valor:'06.00', qnty: 0},
+    item14 = {nome:'Misto quente       ', valor:'10.00', qnty: 0},
+    item15 = {nome:'Brigadeirão        ', valor:'05.00', qnty: 0},
+    item16 = {nome:'Torta de pêssego   ', valor:'07.50', qnty: 0},
+    item17 = {nome:'Torta de maçã      ', valor:'07.50', qnty: 0},
+    item18 = {nome:'Torta de morango   ', valor:'07.50', qnty: 0},
+    item19 = {nome:'Bolo de brigadeiro ', valor:'04.50', qnty: 0},
+    item20 = {nome:'Bolo de chocolate  ', valor:'04.50', qnty: 0},
+    item21 = {nome:'Bolo de fubá       ', valor:'04.50', qnty: 0},
+    item22 = {nome:'Bolo de morango    ', valor:'04.50', qnty: 0},
+    item23 = {nome:'Pão de queijo(100g)', valor:'02.50', qnty: 0},
+    item23 = {nome:'Brownie            ', valor:'07.00', qnty: 0}
 ];
 
 //#region: Imprimindo o cardápio
@@ -58,20 +58,25 @@ function anotarPedido() {
     //Quantidade de itens que o usuário deseja
     quantidade = prompt('Quantos vai querer? ');
 
+    //Aumentando a quantidade do item selecionado
     menu[numPedido].qnty += quantidade;
 
     //Perguntando se o cliente quer mais alguma coisa
     resposta = prompt('Deseja pedir mais alguma coisa? ');
+
+    //Transformando a resposta em minuscula para ter apenas uma verificação
+    resp = resposta.toLowerCase();
 }
 
 //Anotando o pedido pela primeira vez
 anotarPedido();
 
 //Enquanto a resposta for sim ele segue anotando o pedido
-while (resposta == 'sim' || resposta == 'Sim' || resposta == 'SIM') {
+while (resp== 'sim') {
     anotarPedido();
 }
 
+//Subtotal da compra, como os itens que nao foram selecionados se multiplicam por ZERO e viram ZERO, não altera o valor total REAL
 const subtotal = ((menu[00].valor * menu[00].qnty) + (menu[01].valor * menu[01].qnty) + (menu[02].valor * menu[02].qnty) + (menu[03].valor * menu[03].qnty) + (menu[04].valor * menu[04].qnty) + (menu[05].valor * menu[05].qnty) + (menu[06].valor * menu[06].qnty) + (menu[07].valor * menu[07].qnty) + (menu[08].valor * menu[08].qnty) + (menu[09].valor * menu[09].qnty) + (menu[10].valor * menu[10].qnty) + (menu[11].valor * menu[11].qnty) + (menu[12].valor * menu[12].qnty) + (menu[13].valor * menu[13].qnty) + (menu[14].valor * menu[14].qnty) + (menu[15].valor * menu[15].qnty) + (menu[16].valor * menu[16].qnty) + (menu[17].valor * menu[17].qnty) + (menu[18].valor * menu[18].qnty) + (menu[19].valor * menu[19].qnty) + (menu[20].valor * menu[20].qnty) + (menu[21].valor * menu[21].qnty) + (menu[22].valor * menu[22].qnty) + (menu[23].valor * menu[23].qnty));
 
 //Multiplicando o valor do item pela quantidade solicitada
