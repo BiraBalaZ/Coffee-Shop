fc7f7035634323adaa180b4fc22e5c10bc1d858a
BiraBalaZ <85596186+BiraBalaZ@users.noreply.github.com>
2022-11-22 10:43:54 -0300

    Versão 1.0.7 - Correção de bugs+adição de produtos
    Adicionando novos produtos e o tipo deles, como salgado, bebidas quentes e geladas, doces e lanche. formatando também a lista do Menu e corrigindo bugs de itens com nome duplicado.

--- coffeeShop.js
+++ coffeeShop.js
@@ -10,30 +10,36 @@ console.log("   [Horário de funcionamento: 08h às 18h]         |_|     \n\n\n"
     
 //Definindo os itens do nosso Menu/Cardápio e seus valores
 var menu = [
-    item1  = {nome:'Café preto           ', valor:'01.00', qnty: 0},
-    item2  = {nome:'Expresso             ', valor:'02.00', qnty: 0},
-    item3  = {nome:'Cappuccino           ', valor:'12.00', qnty: 0},
-    item4  = {nome:'Mocha                ', valor:'10.00', qnty: 0},
-    item5  = {nome:'Mochaccino           ', valor:'15.00', qnty: 0},
-    item6  = {nome:'Machiato             ', valor:'11.50', qnty: 0},
-    item7  = {nome:'Latte                ', valor:'08.00', qnty: 0},
-    item8  = {nome:'Duplo                ', valor:'05.00', qnty: 0},
-    item9  = {nome:'Frappuccino          ', valor:'17.50', qnty: 0},
-    item10 = {nome:'Café com leite      ', valor:'04.00', qnty: 0},
-    item11 = {nome:'Leite               ', valor:'03.50', qnty: 0},
-    item12 = {nome:'X-Burger            ', valor:'25.00', qnty: 0},
-    item13 = {nome:'Donuts              ', valor:'06.00', qnty: 0},
-    item14 = {nome:'Misto quente        ', valor:'10.00', qnty: 0},
-    item15 = {nome:'Brigadeirão         ', valor:'05.00', qnty: 0},
-    item16 = {nome:'Torta de pêssego    ', valor:'07.50', qnty: 0},
-    item17 = {nome:'Torta de maçã       ', valor:'07.50', qnty: 0},
-    item18 = {nome:'Torta de morango    ', valor:'07.50', qnty: 0},
-    item19 = {nome:'Bolo de cenoura     ', valor:'04.50', qnty: 0},
-    item20 = {nome:'Bolo de chocolate   ', valor:'04.50', qnty: 0},
-    item21 = {nome:'Bolo de fubá        ', valor:'04.50', qnty: 0},
-    item22 = {nome:'Bolo de morango     ', valor:'04.50', qnty: 0},
-    item23 = {nome:'Pão de queijo (100g)', valor:'02.50', qnty: 0},
-    item23 = {nome:'Brownie             ', valor:'07.00', qnty: 0}
+    item01 = {nome:'Café preto           ', valor:'01.00',    qnty: 0,    type:'Bebida quente'},
+    item02 = {nome:'Expresso             ', valor:'02.50',    qnty: 0,    type:'Bebida quente'},
+    item03 = {nome:'Cappuccino           ', valor:'11.90',    qnty: 0,    type:'Bebida quente'},
+    item04 = {nome:'Mocha                ', valor:'10.50',    qnty: 0,    type:'Bebida quente'},
+    item05 = {nome:'Mochaccino           ', valor:'14.90',    qnty: 0,    type:'Bebida quente'},
+    item06 = {nome:'Machiato             ', valor:'11.50',    qnty: 0,    type:'Bebida quente'},
+    item07 = {nome:'Latte                ', valor:'07.90',    qnty: 0,    type:'Bebida quente'},
+    item08 = {nome:'Duplo                ', valor:'04.90',    qnty: 0,    type:'Bebida quente'},
+    item09 = {nome:'Café com leite       ', valor:'04.50',    qnty: 0,    type:'Bebida quente'},
+    item10 = {nome:'Leite               ',  valor:'03.50',    qnty: 0,    type:'Bebida quente'},
+    item11 = {nome:'Frappuccino         ',  valor:'17.50',    qnty: 0,    type:'Bebida gelada'},
+    item13 = {nome:'Refrigerante        ',  valor:'05.50',    qnty: 0,    type:'Bebida gelada'},
+    item14 = {nome:'Suco Natural        ',  valor:'07.90',    qnty: 0,    type:'Bebida gelada'},
+    item15 = {nome:'X-Burger            ',  valor:'24.90',    qnty: 0,    type:'Lanche'       },
+    item16 = {nome:'X-Bacon             ',  valor:'29.90',    qnty: 0,    type:'Lanche'       },
+    item17 = {nome:'X-Salada            ',  valor:'26.50',    qnty: 0,    type:'Lanche'       },
+    item18 = {nome:'Donuts              ',  valor:'05.50',    qnty: 0,    type:'Doce'         },
+    item19 = {nome:'Misto quente        ',  valor:'10.50',    qnty: 0,    type:'Doce'         },
+    item20 = {nome:'Brigadeirão         ',  valor:'04.90',    qnty: 0,    type:'Doce'         },
+    item21 = {nome:'Torta de pêssego    ',  valor:'07.50',    qnty: 0,    type:'Doce'         },
+    item22 = {nome:'Torta de maçã       ',  valor:'05.50',    qnty: 0,    type:'Doce'         },
+    item22 = {nome:'Torta de morango    ',  valor:'06.50',    qnty: 0,    type:'Doce'         },
+    item24 = {nome:'Bolo de cenoura     ',  valor:'07.50',    qnty: 0,    type:'Doce'         },
+    item25 = {nome:'Bolo de chocolate   ',  valor:'09.50',    qnty: 0,    type:'Doce'         },
+    item26 = {nome:'Bolo de fubá        ',  valor:'05.50',    qnty: 0,    type:'Doce'         },
+    item27 = {nome:'Bolo de morango     ',  valor:'04.50',    qnty: 0,    type:'Doce'         },
+    item28 = {nome:'Brownie             ',  valor:'06.90',    qnty: 0,    type:'Doce'         },
+    item29 = {nome:'Pão de queijo (100g)',  valor:'02.50',    qnty: 0,    type:'Salgado'      },
+    item30 = {nome:'Coxinha (100g)      ',  valor:'06.90',    qnty: 0,    type:'Salgado'      },
+    item31 = {nome:'Bola de queijo (Un) ',  valor:'01.50',    qnty: 0,    type:'Salgado'      }
 ];
 
 //Função de anotar o pedido
@@ -58,7 +64,7 @@ function anotarPedido() {
     console.log('');
 
     //Transformando a resposta em minuscula para ter apenas uma verificação
-    resp = resposta.toLowerCase();
+    resp = resposta.toLowerCase().trim();
 }
 
 //Variáveis para descobrir a hora do usuário
@@ -68,7 +74,7 @@ hour = day.getHours();
 //O programa só funciona das 8h às 18h
 if (hour >= 8 && hour <= 22) {
 
-    //#region: Imprimindo o cardápio
+    //#region - Imprimindo o cardápio
         console.log('                        Dê uma olhada no nosso carápio!\n');
         j=1
         for (i = 0; i < menu.length; i += 2) {
@@ -83,16 +89,20 @@ if (hour >= 8 && hour <= 22) {
     anotarPedido();
 
     //Enquanto a resposta for sim ele segue anotando o pedido
-    while (resp == 'sim') { anotarPedido() }
+    while (resp == 'sim') {
+        anotarPedido();
+    }
 
     //Subtotal da compra, como os itens que nao foram selecionados se multiplicam por ZERO e viram ZERO, não altera o valor total REAL
     //Pegando o subtotal ser somado automaticamente sem inclusão das variáveis
     subtotal = 0;
-    for (k = 0; k < menu.length; k += 1) { subtotal += menu[k].valor * menu[k].qnty; }
+    for (k = 0; k < menu.length; k += 1) {
+        subtotal += menu[k].valor * menu[k].qnty;
+    }
 
     //Multiplicando o valor do item pela quantidade solicitada
     console.log(`\nSubTotal: R$ ${subtotal}`);
-}
+} 
 else {
     //Se estiver fora do horário de atendimento, ele dá as seguintes mensagens:
     console.log('Estamos Fechados.');
